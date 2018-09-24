5b212d4883fb2b6f54a4e13aef6462b5e93ec659
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-09-24 15:22:48 -0700

    adds a test for returning the bike after the time limit has elapsed

--- contracts/Bike.sol
+++ contracts/Bike.sol
@@ -4,21 +4,18 @@ import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import './Token.sol';
 
 contract Bike is Ownable {
-  address public renter;
-  address public payee;
   uint public rentalFee;
   uint public rentalTimeInMinutes;
-  mapping (address => uint256) public payers;
-  bool public isRented;
   address public tokenContract;
+  address public renter;
+  address public payee;
+  bool public isRented;
   uint public rentalExpiryTime;
 
-  event Rent(address renter); // time too
+  event Rented(address renter); // time too
   event Returned(address renter, uint rentalExpiryTime, uint currentTime, uint amount);
 
-  // events
   // need to check if the bike's time limit has expired
-  // modifier to only rent if its not already renter (isAvailable)
 
   modifier isRenter() {
     require(msg.sender == renter, "Sender is not the current renter");
@@ -27,6 +24,11 @@ contract Bike is Ownable {
 
   modifier bikeIsRented() {
     require(isRented);
+    _;
+  }
+
+  modifier bikeIsAvailable() {
+    require(!isRented);
     _;
   }
 
@@ -40,30 +42,30 @@ contract Bike is Ownable {
     Token t = Token(_token);
     require(_value == (rentalFee * 3));
     require(t.transferFrom(_from,  this, _value));
-    payers[_from] += _value;
     _rent(_from);
   }
 
-  function _rent(address _renter) internal {
+  function _rent(address _renter) internal bikeIsAvailable {
     // require that the bike is available to be rented
     renter = _renter;
     payee = _renter;
     isRented = true;
     rentalExpiryTime = now + rentalTimeInMinutes * 60;
-    emit Rent(msg.sender);
+    emit Rented(msg.sender);
   }
 
   function returnBike() public isRenter bikeIsRented {
     // if time has elapsed, don't refund their escrow
     Token t = Token(tokenContract);
     uint256 amount = rentalFee * 2;
-    require(t.transfer(payee, amount));
+    if (now <= rentalExpiryTime) {
+      require(t.transfer(payee, amount));  
+    }
     emit Returned(renter, rentalExpiryTime, now, amount);
     renter = address(0);
     isRented = false;
     payee = address(0);
     rentalExpiryTime = 0;
-    
   }
 
   function transferBike(address newRenter) public isRenter bikeIsRented {
--- test/bike.js
+++ test/bike.js
@@ -7,11 +7,14 @@ const {
   RENTAL_TIME_IN_MINUTES,
   ESCROW_AMOUNT
 } = require('../constants');
+const { timeTravel } = require('./helper.js');
 
 let coinInstance;
 let crowdSaleInstance;
 let bike;
 let renter;
+let secondRenter;
+let coinRecipient;
 let decimals;
 let transferee;
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
@@ -24,36 +27,35 @@ contract('Bike', function(accounts) {
     bike = await Bike.deployed();
 
     renter = accounts[2];
+    secondRenter = accounts[5];
     decimals = await coinInstance.decimals();
     await crowdSaleInstance.sendTransaction({ from: renter, value: web3.toWei(1, "ether")});
+    await crowdSaleInstance.sendTransaction({ from: secondRenter, value: web3.toWei(1, "ether")});
   });
 
   it("should be owned by the creator", async () => {
     const expected = accounts[0]; 
-    //const bike = await Bike.deployed();
     const owner = await bike.owner();
     assert.equal(expected, owner);
   });
 
   it("should have a rental fee of 3000 BIKE", async () => {
     const expected = RENTAL_FEE;
-    //const bike = await Bike.deployed();
     const rentalFee = await bike.rentalFee();
     assert.equal(expected, rentalFee);
   });
 
   it(`should have a rental time limit of ${RENTAL_TIME_IN_MINUTES} minutes`, async () => {
     const expected = RENTAL_TIME_IN_MINUTES;
-    //const bike = await Bike.deployed();
     const rentalTimeInMinutes = await bike.rentalTimeInMinutes();
     assert.equal(expected, rentalTimeInMinutes);
   });
 
   it("should allow transferring of credits between users", async () => {
     const expected = 1000;
-    const recipient = accounts[5];
-    coinInstance.transfer(recipient, expected, { from: renter });
-    const balance = await coinInstance.balanceOf.call(recipient);
+    coinRecipient = accounts[7];
+    coinInstance.transfer(coinRecipient, expected, { from: renter });
+    const balance = await coinInstance.balanceOf.call(coinRecipient);
     assert.equal(expected, balance.toNumber());
   });
 
@@ -74,11 +76,8 @@ contract('Bike', function(accounts) {
   it('should let the renter transfer tokens to us', async () => {
     const expected = ESCROW_AMOUNT;
     await coinInstance.approveAndCall(bike.address, ESCROW_AMOUNT, '', { from: renter });
-
     const contractBalance = await coinInstance.balanceOf.call(bike.address);
-    const balance = await bike.payers.call(renter);
     const userBalance = await coinInstance.balanceOf.call(renter);
-    assert.equal(ESCROW_AMOUNT, balance.toNumber());
     assert.equal(ESCROW_AMOUNT, contractBalance);
   });
 
@@ -129,26 +128,34 @@ contract('Bike', function(accounts) {
     assert.equal(expected, rented);
   });
 
-  // it should not allow others to rent the bike when it is already rented
+  it("should rent the bike again", async () => {
+    const expected = ESCROW_AMOUNT;
+    const initialBalance = await coinInstance.balanceOf(bike.address);
+    await coinInstance.approveAndCall(bike.address, ESCROW_AMOUNT, '', { from: secondRenter });
 
+    const contractBalance = await coinInstance.balanceOf.call(bike.address);
+    const userBalance = await coinInstance.balanceOf.call(secondRenter);
+    assert.equal(ESCROW_AMOUNT + initialBalance.toNumber(), contractBalance.toNumber());
+  });
 
-  // it should burn or withhold the escrowed credits if the time limit is surpassed
+  it("should not allow others to rent the bike when it is already rented", async () => {
+    try {
+      await coinInstance.approveAndCall(bike.address, ESCROW_AMOUNT, '', { from: renter });      
+    } catch (err) {
+      const currentRenter = await bike.renter();
+      assert.notEqual(renter, currentRenter);
+    }
+  });
+  
+  // For the time travel bit I used this resource:
+  // https://medium.com/coinmonks/testing-solidity-with-truffle-and-async-await-396e81c54f93
+  it("should not return credits if the rental time limit has passed", async () => {
+    await timeTravel(86400);
+    const expected = await coinInstance.balanceOf.call(secondRenter);
+    await bike.returnBike({ from: secondRenter });
+    const newBalance = await coinInstance.balanceOf.call(secondRenter);
+    const currentRenter = await bike.renter();
+    assert.equal(expected, newBalance.toNumber());
+    assert.equal(ZERO_ADDRESS, currentRenter);
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
