9984a4254592c4b8bd2c8b32973fff4aba45b3a3
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-09-24 11:41:03 -0700

    merge the two repos together for easier development

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,30 @@
+const BikeCoinCrowdsale = artifacts.require('./BikeCoinCrowdsale.sol');
+const BikeCoin = artifacts.require('./BikeCoin.sol');
 const Bike = artifacts.require('./Bike.sol');
 
 module.exports = function(deployer, network, accounts) {
   const rentalFee = 3000;
   const rentalTimeInMinutes = 360;
-  deployer.deploy(Bike, rentalFee, rentalTimeInMinutes);
+  const rate = new web3.BigNumber(314159);
+  const wallet = accounts[1];
+
+  return deployer
+    .then(() => {
+      deployer.deploy(Bike, rentalFee, rentalTimeInMinutes);
+    })
+    .then(() => {
+      return deployer.deploy(BikeCoin);
+    })
+    .then(() => {
+      return deployer.deploy(
+        BikeCoinCrowdsale,
+        rate,
+        wallet,
+        BikeCoin.address
+      );
+    })
+    .then(() => {
+      bikeCoinInstance = BikeCoin.at(BikeCoin.address);
+      bikeCoinInstance.transferOwnership(BikeCoinCrowdsale.address);
+    });
 };
